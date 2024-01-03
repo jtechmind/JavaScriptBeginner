67ef4efe035e42bd248139780f59d84f17a2c60c
jtech mind <jtechmind@gmail.com>
2024-01-03 21:36:40 +0530

    data type file updated first time

--- DataTypes.js
+++ DataTypes.js
@@ -1,1 +1,7 @@
+/**
+ * Two categories of DataTypes
+ * Fist Category, Primitive Types
+ * Second Category, Objects or References
+ */
 console.log("Data Types in JavaScript");
+console.log("Primitive Types");
