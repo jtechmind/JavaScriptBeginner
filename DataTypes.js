5e67d01dea37f47b345d82a0ba6e721958b3a621
jtech mind <jtechmind@gmail.com>
2024-01-04 14:45:32 +0530

    Primitive data types are beaing used

--- DataTypes.js
+++ DataTypes.js
@@ -5,3 +5,14 @@
  */
 console.log("Data Types in JavaScript");
 console.log("Primitive Types");
+
+// var is one of the keyword to declare a variable in JS.
+var x = 100; // variavle x is created and assigned value 100 to it.
+
+//print the value of x in the console
+console.log(x); // You can just write the variable name
+console.log("Value of x: " + x); // print with some message
+
+// Check the data type of x.
+console.log(typeof x);
+console.log("Data Type of x: " + typeof x);
