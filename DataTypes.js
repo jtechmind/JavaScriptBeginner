d41d8b756ee31a0f1110c8b0aabf05ae1ba99504
jtech mind <jtechmind@gmail.com>
2024-01-09 00:00:02 +0530

    BigInt added to it

--- DataTypes.js
+++ DataTypes.js
@@ -39,3 +39,36 @@ console.log(null === undefined);
 console.log(null == undefined);
 
 // BigInt and Symbol
+/**
+ * BigInt: A new JavaScript Datatype
+ * JavaScript integer accuracy
+ * JS integers are only accurate up to 15 digits:
+ *
+ * In JavaScript, all numbers are stored in a 64-bit floating-point format
+ * (IEEE 754 standard).
+ * With this standard, large integer cannot be exactly represented and will be rounded.
+ * Because of this, JavaScript can only safely represent integers:
+ * Up to 9007199254740991 +(253-1)
+ * and
+ * Down to -9007199254740991 -(253-1).
+ * Integer values outside this range lose precision.
+ */
+var intVariable_x = 999999999999999;
+var intVariable_y = 9999999999999999;
+console.log(intVariable_x);
+console.log(intVariable_y);
+
+/**
+ * How to declare BigInt
+ * To create BigInt, append "n" to the end of integer
+ * Or call BigInt() method.
+ */
+
+let bigIntVariable_x = 1234567890123456789012345n;
+let bigIntVariable_y = BigInt("1234567890123456789012345");
+
+console.log(bigIntVariable_x);
+console.log(bigIntVariable_y);
+
+console.log(typeof bigIntVariable_x);
+console.log(typeof bigIntVariable_y);
