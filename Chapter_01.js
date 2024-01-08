3545c5b69556c16e09c7ee66c42398beb45c02ec
jtech mind <jtechmind@gmail.com>
2024-01-08 22:10:02 +0530

    Variable, Value and Data types in JavaScript

---
+++ Chapter_01.js
@@ -0,0 +1,11 @@
+/**
+ * Variable, Value and Data types in JavaScript
+ */
+
+// Variavle are container for storing information/value/data.
+// Creating a variable in JavaScript is called "declaring" a variable.
+// var, let and const are the keywords used to declare an variable.
+
+// Data Types: Primitive and non-primitive are two categories of data types.
+// In Primitive we store only single value to it.
+// In non-primitive(Object/Reference) we can store complex data.
