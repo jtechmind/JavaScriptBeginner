016ffd595368aafead4bf232fe28e4c63953fcc6
jtech mind <jtechmind@gmail.com>
2024-01-08 23:44:03 +0530

    variable declaration and value assignment

--- Chapter_01.js
+++ Chapter_01.js
@@ -9,3 +9,16 @@
 // Data Types: Primitive and non-primitive are two categories of data types.
 // In Primitive we store only single value to it.
 // In non-primitive(Object/Reference) we can store complex data.
+
+// Creating or declaring a Variable.
+
+// variable is declared and assign "Undefined" to it
+var firstVariable;
+// console.log() method use to print value in console.
+console.log(firstVariable); // output: undefined
+console.log(typeof firstVariable); // typeof keyword is used to find out data type.
+
+// now assign value to the variable usin = operator
+firstVariable = 50;
+console.log(firstVariable); // value 50 is assined
+console.log(typeof firstVariable); // now the type is also changed
