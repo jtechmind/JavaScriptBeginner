12e45bb8ecd856d3b8fdc921226517649da6fc5d
jtech mind <jtechmind@gmail.com>
2024-01-09 18:14:11 +0530

    function in JavaScript with simple example

---
+++ Chapter_03_Functions.js
@@ -0,0 +1,23 @@
+/**
+ * JavaScript Functions.
+ * A function is a block of code design to perform a particular task.
+ * A function is executed when somthing envikes it or call it.
+ */
+
+/**
+ * Defined with "function keyword followed by name followe by parentheses ()"
+ * The code to be executed, by the function is placed inside curly bracket {}.
+ *
+ * function function_name () {}
+ * function function_name (parameter1,parameter2) {}
+ * A function may or may not have parameter
+ *
+ */
+
+// creating or defining a function
+function helloWorld() {
+  console.log("Hello JavaScript Function");
+}
+
+// calling helloWorld() function
+helloWorld();
