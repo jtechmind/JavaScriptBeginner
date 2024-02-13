22f01c71fbf16b363035932d101721ebf5ae5aaa
jtechmind <jtechmind@gmail.com>
2024-02-13 23:48:04 +0530

    Object Creation in JavaScript.

---
+++ Chapter_04_Objects.js
@@ -0,0 +1,13 @@
+/**
+ * Object Creation:
+ * Objects in JavaScript can be created using
+ * literal notation or constructor notation.
+ *
+ * */
+
+// Literal Notation
+
+let myObject = { id: "1", info: "Object one" };
+
+console.log(myObject);
+console.log(myObject.info);
