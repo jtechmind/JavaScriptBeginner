8b67607a4e89da08d1bc772964bd7fc3d6e65633
jtechmind <jtechmind@gmail.com>
2024-02-14 23:56:13 +0530

    create object with a constructor

--- Chapter_04_Objects.js
+++ Chapter_04_Objects.js
@@ -15,3 +15,21 @@ console.log(myObject.info);
 let student = { first_name: "Karan", last_name: "singh", age: 22 };
 console.log("Student Information:");
 console.log(student.first_name + " " + student.last_name + " " + student.age);
+
+/**
+ * Creating object with a constructor
+ * One of the easiest ways to instantiate an object is in JavaScript.
+ * Constructor is nothing but a function
+ */
+
+function vehicale(name, maker, engine) {
+  this.naem = name;
+  this.maker = maker;
+  this.engine = engine;
+}
+
+// now we will use "new" keyword to create an object
+let car = new vehicale("Harrier", "TATA", "1956cc");
+console.log("Car Name: " + car.naem);
+console.log("Maker: " + car.maker);
+console.log("Engine: " + car.engine);
