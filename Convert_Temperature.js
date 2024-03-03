608346ee88a0e5f04d2016a470933093341c9a19
jtechmind <jtechmind@gmail.com>
2024-03-03 22:21:07 +0530

    celsius to kelvin function added

--- Convert_Temperature.js
+++ Convert_Temperature.js
@@ -1,4 +1,5 @@
 // conver celsius to fahrenheit
+// formula f = (c * 1.8) + 32;
 
 function celsiusToFahrenheit(c) {
   return c * 1.8 + 32;
@@ -7,3 +8,13 @@ function celsiusToFahrenheit(c) {
 let celsius = 51.5;
 let converted = celsiusToFahrenheit(celsius);
 console.log(`${celsius} celsius = ${converted} fahrenheit`);
+
+// convert Celsius to Kelvin
+// k = c + 273.15
+
+function celsiusToKelvin(c) {
+  return c + 273.15;
+}
+
+let celsiusToKelvinResult = celsiusToKelvin(celsius);
+console.log(`${celsius} celsius = ${celsiusToKelvinResult} kelvin`);
