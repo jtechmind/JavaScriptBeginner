c02f04abbd0b67fe31f12e8c546c43cec23c43c4
jtechmind <jtechmind@gmail.com>
2024-03-05 18:41:36 +0530

    feet to cm added.

--- Convert_Length.js
+++ Convert_Length.js
@@ -12,6 +12,11 @@ function feetToInches(f) {
   return f * 12;
 }
 
+// Feet to CM
+function feetToCM(f) {
+  return f / 0.032808;
+}
+
 let feet = 11.4;
 
 let meters = feetToMeters(feet).toFixed(4); //
@@ -19,3 +24,6 @@ console.log(`${feet} feet = ${meters} meters`);
 
 let inches = feetToInches(feet);
 console.log(`${feet} feet = ${inches} inches`);
+
+let cm = feetToCM(feet).toFixed(3);
+console.log(`${feet} feet = ${cm} cm`);
