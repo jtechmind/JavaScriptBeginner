02c71786a0a75bc737e3a3c6212ea3fdd13cbf9d
jtechmind <jtechmind@gmail.com>
2024-03-05 18:33:36 +0530

    feet to inches added.

--- Convert_Length.js
+++ Convert_Length.js
@@ -1,12 +1,21 @@
 /**
- * convert fromm feet to other measurements
+ * convert from feet to other measurements
  */
 
+// Feet to meters
 function feetToMeters(f) {
   return f / 3.2808;
 }
 
-let feet = 11;
+// Feet to inches
+function feetToInches(f) {
+  return f * 12;
+}
+
+let feet = 11.4;
 
 let meters = feetToMeters(feet).toFixed(4); //
 console.log(`${feet} feet = ${meters} meters`);
+
+let inches = feetToInches(feet);
+console.log(`${feet} feet = ${inches} inches`);
